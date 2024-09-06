3e6af25d7488c749e6f4588e85bda31192d6f16c
Flinn <fpjiannan@qq.com>
2024-09-06 14:45:55 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿var source = File.ReadAllText(@"D:\圆周率.txt").AsSpan();
+Console.WriteLine("请输入生日,例如19871229");
+var birthday = Console.ReadLine();
+Console.WriteLine(source.IndexOf(birthday.AsSpan()));
